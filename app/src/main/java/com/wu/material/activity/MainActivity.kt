bb387b46e2a2a00c8a98fec672df38afd804fd61
wkq <1571478933@qq.com>
2022-03-21 11:28:20 +0800

    优化Rv

--- app/src/main/java/com/wu/material/activity/MainActivity.kt
+++ app/src/main/java/com/wu/material/activity/MainActivity.kt
@@ -10,10 +10,7 @@ import com.wu.material.activity.lazy.LazyLoadingFragmentActivity
 import com.wu.material.activity.lazy.LazyLoadingFragmentActivity2
 import com.wu.material.activity.livedata.LiveDataActivity
 import com.wu.material.activity.motionlayout.*
-import com.wu.material.activity.rv.ComplicatedRecyclerViewActivity
-import com.wu.material.activity.rv.RecyclerTreeViewActivity
-import com.wu.material.activity.rv.RecyclerViewActivity
-import com.wu.material.activity.rv.StaggeredGridLayoutManagerActivity
+import com.wu.material.activity.rv.*
 import com.wu.material.bdmap.MaterialMapActivity
 import com.wu.material.databinding.ActivityMainBinding
 
@@ -31,21 +28,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, LiveDataActivity::class.java));
         }
         binding.btRv.setOnClickListener {
-            startActivity(Intent(this, RecyclerViewActivity::class.java));
+            startActivity(Intent(this, RecyclerHomeActivityActivity::class.java));
         }
-        binding.btRv2.setOnClickListener {
-            startActivity(Intent(this, ComplicatedRecyclerViewActivity::class.java));
-        }
-        binding.btRv3.setOnClickListener {
-            startActivity(Intent(this, StaggeredGridLayoutManagerActivity::class.java));
-        }
+
         binding.btRv4.setOnClickListener {
             startActivity(Intent(this, CoroutinesActivity::class.java));
         }
 
-        binding.btRv5.setOnClickListener {
-            startActivity(Intent(this, RecyclerTreeViewActivity::class.java));
-        }
+
         binding.btRv6.setOnClickListener {
             startActivity(Intent(this, LazyLoadingFragmentActivity::class.java));
         }
---
+++ app/src/main/java/com/wu/material/activity/rv/RecyclerHomeActivityActivity.kt
@@ -0,0 +1,49 @@
+package com.wu.material.activity.rv
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.wu.material.R
+import com.wu.material.databinding.ActivityRecyclerviewBinding
+import com.wu.material.databinding.ActivityRecyclerviewHomeBinding
+
+
+/**
+ * @author wkq
+ *
+ * @date 2022年02月17日 10:43
+ *
+ *@des
+ *
+ */
+
+class RecyclerHomeActivityActivity : AppCompatActivity() {
+    var binding: ActivityRecyclerviewHomeBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView<ActivityRecyclerviewHomeBinding>(this, R.layout.activity_recyclerview_home)
+
+        binding!!.btRv.setOnClickListener {
+            startActivity(Intent(this, RecyclerViewActivity::class.java));
+        }
+        binding!!.btRv2.setOnClickListener {
+            startActivity(Intent(this, ComplicatedRecyclerViewActivity::class.java));
+        }
+        binding!!.btRv3.setOnClickListener {
+            startActivity(Intent(this, StaggeredGridLayoutManagerActivity::class.java));
+        }
+        binding!!.btRv5.setOnClickListener {
+            startActivity(Intent(this, RecyclerTreeViewActivity::class.java));
+        }
+    }
+
+
+}
