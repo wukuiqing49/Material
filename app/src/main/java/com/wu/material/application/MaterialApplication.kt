f3983bfb7ce075ddc729496bc475da53de87e83b
wkq <1571478933@qq.com>
2022-04-01 17:29:55 +0800

    音频播放

--- app/src/main/java/com/wu/material/application/MaterialApplication.kt
+++ app/src/main/java/com/wu/material/application/MaterialApplication.kt
@@ -2,6 +2,8 @@ package com.wu.material.application
 
 import android.util.Log
 import androidx.multidex.MultiDexApplication
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory
+import com.alibaba.sdk.android.push.notification.BasicCustomPushNotification
 import com.baidu.mapapi.CoordType
 import com.baidu.mapapi.SDKInitializer
 import com.tencent.mmkv.MMKV
@@ -38,6 +40,7 @@ class MaterialApplication : MultiDexApplication() {
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
         SDKInitializer.setCoordType(CoordType.BD09LL);
+        PushServiceFactory.getCloudPushService().
 
     }
 
