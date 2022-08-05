a021ac285c91659ca1971394ed20681f5068149e
wkq <1571478933@qq.com>
2022-08-05 15:40:36 +0800

    修改主页

--- app/src/main/java/com/wu/material/activity/other/OtherActivity.kt
+++ app/src/main/java/com/wu/material/activity/other/OtherActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.baidu.mapapi.CoordType
+import com.baidu.mapapi.SDKInitializer
 import com.wu.material.R
 import com.wu.material.activity.other.bdmap.MaterialMapActivity
 import com.wu.material.activity.other.mmkv.MMKVActivity
@@ -23,6 +25,12 @@ class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        SDKInitializer.initialize(application);
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
+
         initView()
     }
 
--- app/src/main/java/com/wu/material/application/MaterialApplication.kt
+++ app/src/main/java/com/wu/material/application/MaterialApplication.kt
@@ -66,10 +66,10 @@ class MaterialApplication : MultiDexApplication()  {
     //初始化百度地图
     private fun initBaiduMap() {
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
-        SDKInitializer.initialize(this);
+//        SDKInitializer.initialize(this);
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
-        SDKInitializer.setCoordType(CoordType.BD09LL);
+//        SDKInitializer.setCoordType(CoordType.BD09LL);
     }
 
 
